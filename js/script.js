6ce0513ab89afa8fbff39b9887e326e52dae3f95
Oleg Zomko <1288812pl@gmail.com>
2023-06-09 07:19:04 +0200

    adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -13,18 +13,42 @@
     let htmlString = "";
     for (const task of tasks) {
       htmlString += `
-        <li>
-            ${task.content}
+        <li
+        ${task.done ? ' style="text-decoration: line-through"' : ""}
+        >
+        ${task.content}
         </li>
         `;
     }
-    document.querySelector(".js-tasks").innerHTML = htmlString;
+    document.querySelector(".js__tasks").innerHTML = htmlString;
+  };
+
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+
+    render();
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js__newTask").value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+
+    addNewTask(newTaskContent);
   };
 
   const init = () => {
     render();
-    const form = document.querySelector(".js-form");
-    form.addEventListener("submit", "onFormSubmit");
+    const form = document.querySelector(".js__form");
+
+    form.addEventListener("submit", onFormSubmit);
   };
+
   init();
 }
